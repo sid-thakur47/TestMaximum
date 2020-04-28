49e1b4a88bb07efe8d7b0ab702f084085291cc19
sid-thakur47 <sidthakur258@gmail.com>
2020-04-28 14:12:15 +0530

    ADDED:ExtendedMaximum file

--- Junit/src/test/java/com/bl/MaximumTest.java
+++ Junit/src/test/java/com/bl/MaximumTest.java
@@ -22,22 +22,38 @@ public class MaximumTest {
         String max = maximum.findMaximum( "xyz", "abc", "ghi" );
         Assert.assertEquals( "xyz", max );
     }
-
     @Test
     public void givenThreeIntegersInConstructor_Should_ReturnMaximum() {
-        int maxInt = new Maximum<Integer>( 10, 20, 30,40).findMaximum();
-        Assert.assertEquals( 40, maxInt );
+        int maxInt = new Maximum<>( 10, 20, 30 ).findMaximum();
+        Assert.assertEquals( 30, maxInt );
     }
 
     @Test
+    public void givenThreeIntegersInVariableConstructor_Should_ReturnMaximum() {
+        int maxInt = new ExtendedMaximum<>( 10, 20, 30, 40 ).findMaximum();
+        Assert.assertEquals( 40, maxInt );
+    }
+    @Test
     public void givenThreeFloatsInConstructor_Should_ReturnMaximum() {
-        float maxfloat = new Maximum<Float>( 10.4f, 20.34f, 30.45f,50f ).findMaximum();
+        float maxfloat = new Maximum<Float>( 10.4f, 20.34f, 30.45f ).findMaximum();
+        Assert.assertEquals( 30.45f, maxfloat, 0.01 );
+    }
+
+    @Test
+    public void givenThreeFloatsInVariableConstructor_Should_ReturnMaximum() {
+        float maxfloat = new ExtendedMaximum<>( 10.4f, 20.34f, 30.45f, 50f ).findMaximum();
         Assert.assertEquals( 50.f, maxfloat, 0.01 );
     }
 
     @Test
     public void givenThreeStringsInConstructor_Should_ReturnMaximum() {
-        String maxString = new Maximum<String>( "xyz", "abc", "def","zzz" ).findMaximum();
+        String maxString = new Maximum<>( "xyz", "abc", "def" ).findMaximum();
+        Assert.assertEquals( "xyz", maxString );
+    }
+
+    @Test
+    public void givenThreeStringsInVariableConstructor_Should_ReturnMaximum() {
+        String maxString = new ExtendedMaximum<>( "xyz", "abc", "def", "zzz" ).findMaximum();
         Assert.assertEquals( "zzz", maxString );
     }
 }
