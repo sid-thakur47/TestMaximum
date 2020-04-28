494ad39093c544e9ac421c50adae644c9edea1e2
sid-thakur47 <sidthakur258@gmail.com>
2020-04-28 10:29:36 +0530

    RFACTORED:To use one generic method

--- Junit/src/main/java/com/bl/Maximum.java
+++ Junit/src/main/java/com/bl/Maximum.java
@@ -1,41 +1,15 @@
 package com.bl;
 
-public class Maximum {
+public class Maximum<T> {
 
-    public int findMaximum(int first, int second, int third) {
-        if (first > second) {
-            if (first > third) {
-                return first;
-            } else {
-                return third;
-            }
-        } else if (second > third) {
-            return second;
+    public static <T extends Comparable> T findMaximum(T first, T second, T third) {
+        T maximum = first;
+        if (second.compareTo( maximum ) > 0) {
+            maximum = second;
         }
-        return third;
-    }
-    public float findMaximum(float first, float second, float third) {
-        if (first > second) {
-            if (first > third) {
-                return first;
-            } else {
-                return third;
-            }
-        } else if (second > third) {
-            return second;
+        if (third.compareTo( maximum ) > 0) {
+            maximum = third;
         }
-        return third;
-    }
-    public String findMaximum(String first, String second, String third) {
-        if (first.length() > second.length()) {
-            if (first.length() > third.length()) {
-                return first;
-            } else {
-                return third;
-            }
-        } else if (second.length() > third.length()) {
-            return second;
-        }
-        return third;
+        return maximum;
     }
 }
--- Junit/src/test/java/com/bl/MaximumTest.java
+++ Junit/src/test/java/com/bl/MaximumTest.java
@@ -4,22 +4,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumTest {
+    Maximum maximum = new Maximum();
+
     @Test
     public void givenThreeInteger_Should_ReturnMaximum() {
-        Maximum maximum = new Maximum();
+
         int max = maximum.findMaximum( 10, 20, 30 );
         Assert.assertEquals( 30, max );
     }
     @Test
     public void givenThreeFloat_Should_ReturnMaximum() {
-        Maximum maximum = new Maximum();
         float max = maximum.findMaximum( 10.14f, 20.34f, 30.45f );
         Assert.assertEquals( 30.45f, max, 0.01 );
     }
     @Test
     public void givenThreeString_Should_ReturnMaximum() {
-        Maximum maximum = new Maximum();
-        String max = maximum.findMaximum( "hello", "sid", "hi" );
-        Assert.assertEquals( "hello", max );
+        String max = maximum.findMaximum( "xyz", "abc", "ghi" );
+        Assert.assertEquals( "xyz", max );
     }
 }
