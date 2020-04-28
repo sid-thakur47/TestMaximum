4ad7e28780d5b597674abe7e5fea478ce0920136
sid-thakur47 <sidthakur258@gmail.com>
2020-04-28 12:47:39 +0530

    REFACTORED:To use generic class and find maximum

--- Junit/src/main/java/com/bl/Maximum.java
+++ Junit/src/main/java/com/bl/Maximum.java
@@ -1,6 +1,16 @@
 package com.bl;
 
-public class Maximum<T> {
+public class Maximum<T extends Comparable<T>> {
+    T first, second, third;
+
+    public Maximum(T first, T second, T third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public Maximum() {
+    }
 
     public static <T extends Comparable> T findMaximum(T first, T second, T third) {
         T maximum = first;
@@ -11,5 +21,8 @@ public class Maximum<T> {
             maximum = third;
         }
         return maximum;
+    }
+    public T findMaximum() {
+        return findMaximum( first, second, third );
     }
 }
--- Junit/src/test/java/com/bl/MaximumTest.java
+++ Junit/src/test/java/com/bl/MaximumTest.java
@@ -22,4 +22,22 @@ public class MaximumTest {
         String max = maximum.findMaximum( "xyz", "abc", "ghi" );
         Assert.assertEquals( "xyz", max );
     }
+
+    @Test
+    public void givenThreeIntegersInConstructor_Should_ReturnMaximum() {
+        int maxInt = new Maximum<>( 10, 20, 30 ).findMaximum();
+        Assert.assertEquals( 30, maxInt );
+    }
+
+    @Test
+    public void givenThreeFloatsInConstructor_Should_ReturnMaximum() {
+        float maxfloat = new Maximum<>( 10.4f, 20.34f, 30.45f ).findMaximum();
+        Assert.assertEquals( 30.45, maxfloat, 0.01 );
+    }
+
+    @Test
+    public void givenThreeStringsInConstructor_Should_ReturnMaximum() {
+        String maxString = new Maximum<>( "xyz", "abc", "def" ).findMaximum();
+        Assert.assertEquals( "xyz", maxString );
+    }
 }
